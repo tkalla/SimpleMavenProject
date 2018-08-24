1d7beaca2220fb6444df311565913b714c1dd2e0
Tony Kallakavumkal <tkallakavumkal@gmail.com>
2018-08-24 12:16:51 -0400

    maven project

---
+++ src/main/java/tony/FirstProgram.java
@@ -0,0 +1,12 @@
+package tony;
+
+import org.apache.commons.lang.StringUtils;
+
+public class FirstProgram {
+
+	  public static void main(String[] str) {
+		  
+		   System.out.println("Testing Maven Enabled Project...");
+		   System.out.println(StringUtils.trimToEmpty("  print without spaces..     "));
+	  }
+}
